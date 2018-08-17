3116fb2d462d26e138975b5f847caca3161e7ae3
Hugo Carreño <hugo.carreno@taisachile.cl>
2018-08-17 19:23:56 -0300

    first commit

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'database_port' => '',
+    'database_name' => 'nei8b05bt9a9ic4n',
+    'database_user' => 'tfm3xzf3clpgdrsy',
+    'database_password' => 'mg07wxqz5pqonhcd',
+    'database_prefix' => 'dce_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'r4vqRb6nXESShk4B6QvLmdYAjhDnfQzQ7KP3cXb7XQgwoFvvjdLCAkJz',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-08-17',
+    'locale' => 'es-ES',
+    'cookie_key' => 'sbEtMNO6rcNQc2CxQKLuQoiMaH1fLAEca7hEnV1dAJRmmEH9VkR0pEt8',
+    'cookie_iv' => 'bZPDa4xV',
+    'new_cookie_key' => 'def0000074142bb4052a5f88c4ce38b55aa91663469151838b79d36a365a2163810973faded8f1118b87d15093508b2fa7b07d2c4d8c0d3fc6bf60316c26f967bf5e67f1',
+  ),
+);
